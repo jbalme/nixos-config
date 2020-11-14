d27087165cfd17192d47b26b21eaf1a970e82e08
Jonathan Baldwin <jbal@jbal.me>
2020-11-13 21:38:34 -0500

    nixfmt

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -1,11 +1,10 @@
 { pkgs, lib, ... }:
-with lib;
-{
-    # Newer kernels tend to have better AMDGPU support.
-    boot.kernelPackages = with pkgs; mkDefault linuxPackages_latest;
-    
-    hardware.opengl = rec {
-        extraPackages = with pkgs; [ amdvlk ];
-        extraPackages32 = extraPackages;
-    };
+with lib; {
+  # Newer kernels tend to have better AMDGPU support.
+  boot.kernelPackages = with pkgs; mkDefault linuxPackages_latest;
+
+  hardware.opengl = rec {
+    extraPackages = with pkgs; [ amdvlk ];
+    extraPackages32 = extraPackages;
+  };
 }
