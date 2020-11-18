b55b47ca3add2b0ef904a55b114d2e6df449103e
Jonathan Baldwin <jbal@jbal.me>
2020-11-18 18:07:28 -0500

    add memtest

--- modules/hardware/disks/boot.nix
+++ modules/hardware/disks/boot.nix
@@ -1,18 +1,29 @@
 { pkgs, ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.initrd.luks.devices.crypt0.device = "/dev/disk/by-partlabel/LINUX";
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot = {
+    loader.systemd-boot = {
+      enable = true;
+      consoleMode = "max";
+      memtest86.enable = true;
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-partlabel/EFIBOOT";
-    fsType = "vfat";
+    initrd = {
+      luks.devices.crypt0.device = "/dev/disk/by-partlabel/LINUX";
+      availableKernelModules =
+        [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+      kernelModules = [ "dm-snapshot" ];
+    };
   };
 
-  fileSystems."/" = {
-    device = "/dev/mapper/vg0-root";
-    fsType = "ext4";
+  fileSystems = {
+    "boot" = {
+      device = "/dev/disk/by-partlabel/EFIBOOT";
+      fsType = "vfat";
+    };
+
+    "/" = {
+      device = "/dev/mapper/vg0-root";
+      fsType = "ext4";
+    };
   };
 
   swapDevices = [{ device = "/dev/mapper/vg0-swap"; }];
