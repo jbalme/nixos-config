8b7964a72a1c5f6abcbf177fe8bb4878540c697b
Jonathan Baldwin <jbal@jbal.me>
2020-11-19 19:48:25 -0500

    AMD early kms

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -2,9 +2,7 @@
 with lib; {
   # Newer kernels tend to have better AMDGPU support.
   boot.kernelPackages = with pkgs; mkDefault linuxPackages_latest;
-
-  hardware.opengl = rec {
-    extraPackages = with pkgs; [ amdvlk ];
-    extraPackages32 = extraPackages;
-  };
+  
+  # Enable early KMS.
+  boot.initrd.kernelModules = [ "amdgpu" ];
 }
