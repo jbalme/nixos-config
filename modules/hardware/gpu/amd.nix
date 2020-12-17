9558be3035f9b34030d95ecaf0718672d86d65ee
Jonathan Baldwin <jbal@jbal.me>
2020-12-17 15:38:52 -0500

    Use Zen kernel for AMD.

--- modules/hardware/gpu/amd.nix
+++ modules/hardware/gpu/amd.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }:
 with lib; {
   # Newer kernels tend to have better AMDGPU support.
-  boot.kernelPackages = with pkgs; mkDefault linuxPackages_latest;
+  boot.kernelPackages = with pkgs; mkDefault linuxPackages_zen;
   
   # Enable early KMS.
   boot.initrd.kernelModules = [ "amdgpu" ];
